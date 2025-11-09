438e000a35d9b757e4396c1810045607b8dd607c
nothing36 <prodipsarker953@gmail.com>
2025-11-09 12:17:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-gray-300'>Tailwind go</h1>
+    </div>
+  );
+};
+
+export default App;
